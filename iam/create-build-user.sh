1d98c6d1b9618d3e7c1b7fb4b5592c0c17567f58
Mike Thomas <mathomas@users.noreply.github.com>
2017-09-07 13:30:35 -0600

    Fix file references, for reals.

--- iam/create-build-user.sh
+++ iam/create-build-user.sh
@@ -11,13 +11,13 @@ fi
 
 OWNER=$1
 PROJECT=$2
-
-aws iam create-user --user-name "${OWNER}-${PROJECT}" > created-user-out-credentials.json
-aws iam create-access-key --user-name "${OWNER}-${PROJECT}" >> created-user-out-credentials.json
-aws iam put-user-policy --user-name "${OWNER}-${PROJECT}" --policy-name Ecr --policy-document file://scripts/EcrPolicy.json
-aws iam put-user-policy --user-name "${OWNER}-${PROJECT}" --policy-name Ecs --policy-document file://scripts/EcsPolicy.json
-aws iam put-user-policy --user-name "${OWNER}-${PROJECT}" --policy-name S3 --policy-document file://scripts/S3Policy.json
-aws iam put-user-policy --user-name "${OWNER}-${PROJECT}" --policy-name Ssm --policy-document file://scripts/SsmPolicy.json
+USER_NAME="${OWNER}-${PROJECT}"
+aws iam create-user --user-name "${USER_NAME}" > created-user-out-credentials.json
+aws iam create-access-key --user-name "${USER_NAME}" >> created-user-out-credentials.json
+aws iam put-user-policy --user-name "${USER_NAME}" --policy-name Ecr --policy-document file://iam/EcrPolicy.json
+aws iam put-user-policy --user-name "${USER_NAME}" --policy-name Ecs --policy-document file://iam/EcsPolicy.json
+aws iam put-user-policy --user-name "${USER_NAME}" --policy-name S3  --policy-document file://iam/S3Policy.json
+aws iam put-user-policy --user-name "${USER_NAME}" --policy-name Ssm --policy-document file://iam/SsmPolicy.json
 
 echo
 echo "Look in created-user-out-credentials.json for generated values."
